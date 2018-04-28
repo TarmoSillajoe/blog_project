773e4eda1f0f3293656582fa33c7fa4ad10dcf22
tsillajoe@gmail.com <tsillajoe@gmail.com>
2018-04-28 12:24:13 +0300

    model User and authentication added to accounts

--- simplesocial/accounts/admin.py
+++ simplesocial/accounts/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here. No need to registe User, because it's built in Django.
--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.core.urlresolvers import reverse_lazy
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-form . import forms
+from . import forms
+
 # Create your views here.
 class SignUp(CreateView):
     form_class = forms.UserCreateForm
