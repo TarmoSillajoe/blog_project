945fae157d645aa1a02092cbd369ed8a9483acd3
tsillajoe@gmail.com <tsillajoe@gmail.com>
2018-04-29 15:59:39 +0300

    Registered model 'Group' in admin.py and added clas GroupMemberInline to admin.py"

--- simplesocial/groups/admin.py
+++ simplesocial/groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+# This class allows editing groupmembers under group in admin view.
+class GroupMemberInline(admin.TabularInline):
+    model = models.GroupMember
+
+admin.site.register(models.Group)
